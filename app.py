ef29ff2cb1fc5ffe58350993c37fe3e4c737767b
Andrew Fiel <andrew.w.fiel@gmail.com>
2020-04-04 15:22:30 -0600

    basic server test

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
+powered = False
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("base.html")
+
+
+@app.route('/api/signalOn', methods=['POST'])
+def outletOn():
+    return jsonify({'newState': "on",
+                    'success': True})
+
+
+@app.route('/api/signalOff', methods=['POST'])
+def outletOff():
+    return jsonify({'newState': "off",
+                    'success': True})
+
 
 if __name__ == '__main__':
     app.run()
