17753c219b98ceb5cfefc38d049b162fbf488001
Andrew Fiel <andrew.w.fiel@gmail.com>
2020-04-26 10:49:37 -0600

    Web UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, make_response
 from redditBot import selfPost
 import os
 app = Flask(__name__)
@@ -7,22 +7,33 @@ powered = False
 
 
 @app.route('/')
-def hello():
-    return render_template("base.html")
+@app.route('/login')
+def login():
+    if (request.authorization and request.authorization.username == os.environ['MC_USER'] and request.authorization.password == os.environ['MC_PASS']):
+        return render_template("base.html")
+    return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 
 @app.route('/api/reddit', methods=['POST'])
 def outletOn():
-    return processRedditRequest(request.form['key'], request.form['concentration'])
+    if (validateRedditRequest(request)):
+        selfPost(int(request.form['concentration']))
+        return jsonify({'success': 'true'})
+    return({'success': 'false'})
 
 
-def processRedditRequest(key, cl):
-    if (key == os.environ['REQUEST_KEY']):
-        selfPost(int(cl))
-        success = 'true'
-    else:
-        success = 'false'
-    return jsonify({'success': success})
+def validateRedditRequest(req):
+    if (req.form['key'] == os.environ['REQUEST_KEY']):
+        return True
+    return False
+
+
+@app.route('/redditButtonOnClick', methods=['GET'])
+def redditButtonOnClick():
+    if request.authorization and request.authorization.username == os.environ['MC_USER'] and request.authorization.password == os.environ['MC_PASS']:
+        val = request.args['value']
+        selfPost(int(val))
+    return ("Must be logged in to use this resource.")
 
 
 if __name__ == '__main__':
